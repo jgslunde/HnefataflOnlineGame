3be5d35251db79e66398e498035cc00962d77d02
jgslunde <jonas.g.s.lunde@gmail.com>
2025-11-13 00:09:05 +0100

    typo

--- mctsAgent.js
+++ mctsAgent.js
@@ -10,7 +10,7 @@ class MCTSAgent {
         this.mcts = null;
         this.isLoading = false;
         this.isReady = false;
-        this.currentModelPath = 'checkpoints/checkpoint_iter_99.onnx'; // Default model
+        this.currentModelPath = 'checkpoints/checkpoint_small_iter_150.onnx'; // Default model
         
         console.log("[MCTSAgent] Created, waiting for initialization");
     }
